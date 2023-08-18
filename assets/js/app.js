0b07257c2a4a441ec943341ae752d4854becb487
antonio-maldonado <67517910+antonio-maldonado@users.noreply.github.com>
2023-08-18 11:05:21 -0600

    ci: agregamos comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,39 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-// const usersEndpoint = "../json/user.json"
-let n = document.querySelector('.name');
+
+//Se le cambió el nombre a las variables $n,$b,$l por -> n,b,l
+let n = document.querySelector('.name'); //Se cambio el selector de tipo por el selector de clase
 const b = document.querySelector('#blog');
 const l = document.querySelector('.location');
 
+//Se cambió function por una variable para evitar el hoisting
 const displayUser = async ( username ) => {
-  n.textContent = 'cargando...';
+  n.textContent += 'cargando...'; //Concatenamos el contenido para no perder lo escrito previamente
   let response,data;
   try{
      response = await fetch(`${usersEndpoint}/${username}`);
   }catch(error){
-    console.warn("Hubo un error");
+    handleError(error); //Si hay error ejecutamos la funcion error
   }
 
   try {
-    
      data = await response.json();
   } catch (error) {
-    console.warn("Hubo un error");
+    handleError(error); //Si hay error ejecutamos la funcion error
   }
 
-  n.textContent = `${data.name}`;
-  b.textContent = `${data.blog}`;
-  l.textContent = `${data.location}`;
+  n.textContent += `${data.name}`; //Concatenamos el contenido para no perder lo escrito previamente
+  b.textContent += `${data.blog}`;//Concatenamos el contenido para no perder lo escrito previamente
+  l.textContent += `${data.location}`;//Concatenamos el contenido para no perder lo escrito previamente
 }
 
+//Se cambió function por una variable para evitar el hoisting
 const handleError =  ( err ) => {
   console.log('OH NO!');
-  let n = document.querySelector('.name');
+  //n = document.querySelector('.name');  //Se eliminó ya que no era necesario porque es variable global
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent += `Algo salió mal: ${err}`//Concatenamos el contenido para no perder lo escrito previamente
 }
 
-displayUser('stolinski').catch(handleError);
+//Se eliminó catch porque se está usando async-await con try-catch
+displayUser('stolinski');
